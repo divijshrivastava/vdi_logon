89633a07ad65e014df68378d510695da7bb25f2c
Divij <divij.shrivastava@gmail.com>
2020-12-03 17:10:38 +0530

    put script for refreshing

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
 window.onload = ()=>{
-    console.log("content script is working");
+
+    if(document.location.href.includes("tiaa-cref")){
+        console.log("Tiaa-cref window. making changes.")
+        let refresh = ()=>{let msba = document.querySelectorAll(".messageBoxAction");msba.forEach(ms=>{ms.querySelectorAll(".default").forEach(d=>d.click());})}
+
+    let interval =  setInterval(refresh, 10000);
+    }
+
 }
