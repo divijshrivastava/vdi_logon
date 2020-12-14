36495cd81479e4f2727cd23dcd64f0eb1c1806d4
Divij <divij.shrivastava@gmail.com>
2020-12-14 15:18:32 +0530

    removed default download

--- content.js
+++ content.js
@@ -2,9 +2,9 @@ window.onload = ()=>{
 
     if(document.location.href.includes("tiaa-cref")){
         console.log("Tiaa-cref window. making changes.")
-        let refresh = ()=>{let msba = document.querySelectorAll(".messageBoxAction");msba.forEach(ms=>{ms.querySelectorAll(".default").forEach(d=>d.click());})}
-
-    let interval =  setInterval(refresh, 10000);
+        let refresh = ()=>{let msba = document.querySelectorAll(".messageBoxAction");
+        msba.forEach(ms=>{ms.querySelectorAll(".default").forEach(d=>d.click());})}
+        let interval =  setInterval(refresh, 10000);
     }
 
 }
