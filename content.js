73735348f2e70b7dfaba9b5b46e2607194425aa8
Divij <divij.shrivastava@gmail.com>
2024-05-06 16:36:53 +0530

    changes according to ms vdi

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 window.onload = ()=>{
 
-    if(document.location.href.includes("tiaa-cref")){
-        console.log("Tiaa-cref window. making changes.")
-        let refresh = ()=>{let msba = document.querySelectorAll(".messageBoxAction");
-        msba.forEach(ms=>{ms.querySelectorAll(".default").forEach(d=>d.click());})}
-        let interval =  setInterval(refresh, 10000);
+    if(document.location.href.includes("morganstanley")){
+        console.log("morganstanley window. making changes.")
+        let refresh = ()=>{
+            window.location.reload();   
+        }
+        let interval =  setInterval(refresh, 120000);
     }
 
 }
